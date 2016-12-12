9b39297929bfb87e41c174fee60c3f66d264071b
Alan <alan@ciandt.com>
2016-12-12 09:25:55 -0200

    fix gulp files

--- gulpfile.js
+++ gulpfile.js
@@ -3,20 +3,27 @@ var sass = require('gulp-sass');
 var watch = require('gulp-watch');
 var browserSync = require('browser-sync').create();
 
-gulp.task('default', function() {
-  watch('sass/**/*.scss', function () {
-    gulp.src('sass/**/*.scss')
+gulp.task('sass', function () {
+ return gulp.src('./sass/**/*.scss')
+   .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+   .pipe(gulp.dest('./css'));
+});
+
+gulp.task('default', ['sass'], function() {
+  watch('./sass/**/*.scss', function () {
+    return gulp.src('./sass/**/*.scss'.on('error', sass.logError))
       .pipe(sass())
-      .pipe(gulp.dest('css'))
+      .pipe(gulp.dest('./css'))
       .pipe(browserSync.stream());
   });
-  watch('index.html', function () {
-    browserSync.reload("index.html");
+  watch('./index.html', function () {
+    browserSync.reload("./index.html");
   });
 
   browserSync.init({
           server: {
               baseDir: "./"
           }
-      });
+  });
+
 });
